e046661b43ebb59b9fe8dd3f7fcfb7181dc39839
VAVS87 <vanessa.valenzuela.s@icloud.com>
2025-02-17 19:19:51 -0300

    se crea el array y la funcion de agregar

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos =[];
+
+//se crea la funcion para limpiar caja
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+
+// crear una funcion que agregue el nombre al array
+
+function agregarAmigo(){
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim(); 
+    //verificando que el valor no sea vacio,dando una alerta si es asi
+    if (nombre !== ""){
+        listaAmigos.push(nombre)  
+        console.log(listaAmigos)
+        limpiarCaja();
+
+    }else
+    alert ("Ingrese un nombre valido")
+    return;
+
+    
+}
